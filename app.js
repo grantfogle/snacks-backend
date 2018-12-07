0e4228417dfbf50516bf46d75c13547151ee434a
grantfogle <grantjfogle@gmail.com>
2018-12-07 11:08:50 -0700

    set up server data

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+const queries = require('./queries')
 
 app.get('/', (req, res) => {
-    res.send('dude it is gucci');
+    queries.listAllSnacks().then(snacks => res.send(snacks));
 })
 
 app.listen(port, console.log(`Server running on ${port}`));
