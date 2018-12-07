64a8dcf9832b2d57cdcfd1fa64534776a8ba5914
grantfogle <grantjfogle@gmail.com>
2018-12-07 11:32:35 -0700

    deploying to heroku

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
     queries.listAllSnacks().then(snacks => res.send(snacks));
 })
 
+app.get('/:id', (req, res) => {
+    queries.getSnacksById(req.params.id).then(snacks => res.send(snacks));
+})
+
 app.listen(port, console.log(`Server running on ${port}`));
--- queries.js
+++ queries.js
@@ -3,5 +3,11 @@ const database = require('./database-connection');
 module.exports = {
     listAllSnacks() {
         return database('snacks');
-    }
+    },
+    getSnacksById(id) {
+        return (
+            database('snacks').where('id', id)
+            // database('reviews').where('snack_id', id)
+        )
+    },
 }
